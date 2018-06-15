08d929c22c80a0e451c68ef69456f9d377c02dd8
matteonm <matteonm@cim.mcgill.ca>
2018-06-15 16:45:46 -0400

    Fully working working multiplayer mode 1

--- index.js
+++ index.js
@@ -12,27 +12,37 @@ var io = socket(server);
 
 io.on('connection', function(socket){
     console.log('Made socket connection with: ', socket.id);
+    console.log('--------------------------------------------------------------');
 
     // Handle generate event
     socket.on('generate', function(){
         var text = "";
         var possible = "abcd";
 
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (var i = 0; i < 18; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
         console.log('Generated new sequence: ', text);
         io.sockets.emit('generate', text);
     });
 
-    socket.on('shoe', function(data) {
-        io.sockets.emit('shoe', data);
+    socket.on('shoe', function(player, finished) {
+        console.log('!!! player = ' + player);
+        console.log('!!! finished = ' + finished);
+        io.sockets.emit('shoe', player, finished);
     });
 
-    socket.on('fail', function() {
-        io.sockets.emit('fail', {})
+    socket.on('fail', function(loser) {
+        io.sockets.emit('fail', loser)
     });
 
     socket.on('startnew', function() {
         io.sockets.emit('startnew', {})
+    });
+
+    socket.on('startfail', function() {
+        io.sockets.emit('startfail', {})
     });
 
 });
